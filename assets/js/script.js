c69838dd62295a1365e0bf7686e73642589ebb26
AliceOhio <alicekulcsarpenner@gmail.com>
2023-02-01 13:04:24 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+    
     runGame("addition");
 })
 
@@ -35,6 +41,8 @@ function runGame(gameType) {
         alert (`Unknown game type: ${gameType}`);
         throw `Unknwon game type: ${gameType}. Aborting!`
     }
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 }
 
 /**
